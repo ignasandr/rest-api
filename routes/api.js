ecf995c9d41c464b5ca0f49e8f46446e1ab64008
Ignas Andriuškevičius <63236361+ignasandr@users.noreply.github.com>
2020-05-21 21:57:57 +0300

    INIT (router, mongoose, request handling)

---
+++ routes/api.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const Ninja = require('../models/ninja');
+
+//get a list of ninjas from the db
+router.get('/ninjas', (req, res) => {
+    res.send({type: 'GET'});
+});
+
+//add a new ninja to the db
+router.post('/ninjas', (req, res) => {
+    Ninja.create(req.body).then(ninja => {
+        res.send(ninja)
+    });
+});
+
+//update a ninja in the db
+router.put('/ninjas/:id', (req, res) => {
+    res.send({type: 'PUT'});
+});
+
+//delete a ninja from the db 
+router.delete('/ninjas/:id', (req, res) => {
+    res.send({type: 'DELETE'});
+});
+
+module.exports = router;

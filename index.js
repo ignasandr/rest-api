061eb4b07b51fc6b9a01dc11872144fe6a582393
Ignas Andriuškevičius <63236361+ignasandr@users.noreply.github.com>
2020-05-22 11:14:20 +0300

    API done

--- index.js
+++ index.js
@@ -6,12 +6,21 @@ const mongoose = require('mongoose');
 const app = express();
 
 //connect to mongodb
-mongoose.connect('mongodb://localhost/ninjago');
-mongoose.Promise = global.Promise;
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useFindAndModify', false);
+mongoose.connect('mongodb://localhost/ninjago', { useNewUrlParser: true });
+// mongoose.Promise = global.Promise;
 
+//middleware for parsing JSON
 app.use(bodyParser.json());
 
+//middleware for initializing routes
 app.use('/api', require('./routes/api'));
+
+//middleware for handiling errors
+app.use(function(err, req, res, next){
+    res.status(422).send({error: err.message});
+});
 
 // listen for requests
 app.listen(process.env.port || 4000, function(){
--- routes/api.js
+++ routes/api.js
@@ -3,25 +3,46 @@ const router = express.Router();
 const Ninja = require('../models/ninja');
 
 //get a list of ninjas from the db
-router.get('/ninjas', (req, res) => {
-    res.send({type: 'GET'});
+router.get('/ninjas', (req, res, next) => {
+    // Ninja.find({}).then(ninjas => {
+    //     res.send(ninjas);
+    // });
+    Ninja.aggregate([{
+        $geoNear: {
+        near: {
+            'type': 'Point',
+            'coordinates': [parseFloat(req.query.lng), parseFloat(req.query.lat)]
+            },
+        distanceField: "dist.calculated",
+        maxDistance: 100000,
+        spherical: true
+        }
+        }]).then((ninjas) => {
+        res.send(ninjas);
+        });
 });
 
 //add a new ninja to the db
-router.post('/ninjas', (req, res) => {
+router.post('/ninjas', (req, res, next) => {
     Ninja.create(req.body).then(ninja => {
         res.send(ninja)
-    });
+    }).catch(next);
 });
 
 //update a ninja in the db
-router.put('/ninjas/:id', (req, res) => {
-    res.send({type: 'PUT'});
+router.put('/ninjas/:id', (req, res, next) => {
+    Ninja.findOneAndUpdate({_id: req.params.id}, req.body).then(() => {
+        Ninja.findOne({_id: req.params.id}).then(ninja => {
+            res.send(ninja);
+        });
+    })
 });
 
 //delete a ninja from the db 
-router.delete('/ninjas/:id', (req, res) => {
-    res.send({type: 'DELETE'});
+router.delete('/ninjas/:id', (req, res, next) => {
+    Ninja.findOneAndRemove({_id: req.params.id}).then(ninja => {
+        res.send(ninja);
+    });
 });
 
 module.exports = router;
